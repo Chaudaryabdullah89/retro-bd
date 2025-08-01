0f2a55a0f68ff15c3b8f668f52a12a165055c053
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-08-01 22:02:06 +0500

    dsf

--- server.js
+++ server.js
@@ -18,17 +18,28 @@ app.use(express.urlencoded({ extended: true }));
 connecttodb();
 
 
-app.get('/', (req, res) => {
-  res.send('Good Back from Retro-BD');
-});
-
 // CORS configuration to accept requests from all origins
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  credentials: false
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  credentials: false,
+  optionsSuccessStatus: 200
 }));
+
+// Add CORS debugging middleware
+app.use((req, res, next) => {
+  console.log('Request origin:', req.headers.origin);
+  console.log('Request method:', req.method);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Good Back from Retro-BD');
+});
 
 app.use('/api', blogroute);
 app.use('/api/verification', PasscodeRoute);
