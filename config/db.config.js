931dc4fa20eaf154cc861edec8dd4dcbc42348e2
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-08-01 21:51:10 +0500

    sa

--- config/db.config.js
+++ config/db.config.js
@@ -4,10 +4,9 @@ dotenv.config();
 
 const connecttodb = async () => {
   try {
-    console.log('Mongo URI:', process.env.MONGO_URL);
+    console.log('Mongo URI:', process.env.MONGO_URL );
     await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+   
     });
     console.log('Connected to MongoDB');
   } catch (error) {
